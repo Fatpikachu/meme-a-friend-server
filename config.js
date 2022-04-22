b0717d31383ab86cc0ad86c1e8fd51fd461233b4
Fatpikachu <jefyleung@gmail.com>
2022-04-21 23:58:11 -0700

    auth key

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ module.exports = {
   IMGUR_ID : 'd6e4594ea5e39d3',
   IMGUR_SECRET : 'da374e13bdfd2a4b5af5bf7c1c0973143fa194ce',
   accountSid : 'AC55cef264f6c96a997f7abaca071cf0ed',
-  authToken : 'cb0d4b35880dc10c136fe0d2fbb230a8',
+  authToken : '9a7c55ff1ca27ac46bab405c68a48345',
 }
